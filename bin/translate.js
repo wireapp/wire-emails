05dcfcb378d3557eee2098f7d394698499c201db
Panayiotis Lipiridis <lipiridis@gmail.com>
2018-03-20 01:19:54 +0200

    Remove ET

--- bin/translate.js
+++ bin/translate.js
@@ -23,7 +23,6 @@ const path = require('path');
 const LANGUAGE_MAPPINGS = {
   en: 'en',
   'de-DE': 'de',
-  'et-EE': 'et',
 };
 
 const IGNORE = [...new Set(Object.keys(LANGUAGE_MAPPINGS).concat(Object.values(LANGUAGE_MAPPINGS)))];
